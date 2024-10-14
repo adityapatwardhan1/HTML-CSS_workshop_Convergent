99fc1828d79485d653559ef63c5cfde4d13a89c5
Aditya Patwardhan <adityap.thecool@gmail.com>
2024-10-14 13:57:02 -0500

    Updates to my website

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // Alert message when the page loads
 window.onload = function() {
-  alert("Welcome to my first web page!");
+  // alert("Welcome to my first web page!");
 };
 
 // Function to change the text when button is clicked
